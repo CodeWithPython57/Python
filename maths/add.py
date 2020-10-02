f4d6c08d6ad19d3fb0d18ac1c03a86c705808a1e
CodeWithPython57 <68240991+CodeWithPython57@users.noreply.github.com>
2020-10-02 18:15:29 +0530

    Add_N_Numbers.py
    Addition of N numbers. If you add any syri

--- maths/add.py
+++ maths/add.py
@@ -1,19 +1,16 @@
 """
-Just to check
+Addition of N numbers.
 """
 
 
-def add(a, b):
-    """
-    >>> add(2, 2)
-    4
-    >>> add(2, -2)
-    0
-    """
-    return a + b
+def add(*a):
+    a=[*a]
+    try:
+        value=sum(a)
+        print('Sum is ' + str(value))
+    except:
+        print('Addition is invalid')
 
-
-if __name__ == "__main__":
-    a = 5
-    b = 6
-    print(f"The sum of {a} + {b} is {add(a, b)}")
+add(2,3) # Output is 5
+add(34,2,1) # Output is 37
+add(5,6,8,1,3) # output is 23
